d18f5ce475a0cc1502591f066b22d1ce70938b4e
Nyaradzo01 <chikumba822@gmail.com>
2022-09-12 11:45:18 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return always 0 success
+ */
+int main(void)
+{
+	Print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
